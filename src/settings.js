5a605c5a23b2041ca9e30cb86af41fb44bb094e0
fangbo <649392395@qq.com>
2021-12-30 14:55:46 +0800

    创建项目

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  title: '董事办书面会议',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false
+}
---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">name: {{ name }}</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Dashboard',
+
+  // eslint-disable-next-line vue/order-in-components
+  data: function() {
+    return {
+      name: '董事办书面会议'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
